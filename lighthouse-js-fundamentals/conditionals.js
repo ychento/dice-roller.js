304e105e362300b8c1ab1658a55af1ef7c50385b
ychento <your@email.comyiyaochen@gmail.com>
2023-05-13 12:55:22 +0000

    Use conditionals to tell people about the weather

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,9 +1,6 @@
-const isCitizen = true;
-const age = 26;
+const temperature = 50;
 
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
-
-
 
